5ffed7cbcbeabd59976e7273a7d350b350eda88f
Siska Dávid (WHDDUM) <90756627+sdavid172@users.noreply.github.com>
2023-04-13 09:35:44 +0200

    Add files via upload

---
+++ pong/src/main.c
@@ -0,0 +1,20 @@
+#include "game.h"
+
+/**
+ * Main function
+ */
+int main(int argc, char* argv[])
+{
+    Game game;
+    Pong pong;
+
+    init_game(&game, 800, 600);
+    while (game.is_running) {
+        handle_game_events(&game,&pong);
+        update_game(&game);
+        render_game(&game);
+    }
+    destroy_game(&game);
+
+    return 0;
+}
