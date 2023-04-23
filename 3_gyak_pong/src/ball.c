65dd4d9a32b18f8a8d74d39d7ce9bc96489ec19a
Siska Dávid (WHDDUM) <90756627+sdavid172@users.noreply.github.com>
2023-04-23 11:32:07 +0200

    Add files via upload

---
+++ 3_gyak_pong/src/ball.c
@@ -0,0 +1,67 @@
+#include "ball.h"
+
+#include <GL/gl.h>
+
+
+
+#include <math.h>
+
+void init_ball(Ball* ball, float x, float y)
+{
+    ball->x = x;
+    ball->y = y;
+    ball->radius = 50;
+    ball->speed_x = 200;
+    ball->speed_y = 200;
+}
+
+
+
+void update_ball(Ball* ball, double time)
+{
+    ball->x += ball->speed_x * time;
+    ball->y += ball->speed_y * time;
+}
+
+
+void ball_size (Ball* ball, float size)
+{
+
+ball->radius += size;
+
+if(ball->radius == 150)
+{
+ball->radius = 50;
+}
+
+}
+
+void ball_pos (Ball*ball, float xp, float yp)
+{
+
+    ball->x = xp;
+    ball->y = yp;
+}
+
+
+void render_ball(Ball* ball)
+{
+    double angle;
+    double x;
+    double y;
+
+    glPushMatrix();
+    glTranslatef(ball->x, ball->y, 0.0);
+    glBegin(GL_TRIANGLE_FAN);
+    glColor3f(1.0, 0.9, 0.8);
+    glVertex2f(0, 0);
+    angle = 0;
+    while (angle < 2.0 * M_PI + 1) {
+        x = cos(angle) * ball->radius;
+        y = sin(angle) * ball->radius;
+        glVertex2f(x, y);
+        angle += 0.8;
+    }
+    glEnd();
+    glPopMatrix();
+}
---
+++ 3_gyak_pong/src/pong.c
@@ -0,0 +1,102 @@
+#include "pong.h"
+#include <stdio.h>
+
+void init_pong(Pong* pong, int width, int height)
+{
+    pong->width = width;
+    pong->height = height;
+    init_pad(&(pong->left_pad), 0, height, RED_THEME);
+    init_pad(&(pong->right_pad), width - 50, height, GREEN_THEME);
+    init_ball(&(pong->ball), width / 2, height / 2);
+    pong->player1_score = 0;
+    pong->player2_score = 0;
+}
+
+void update_pong(Pong* pong, double time)
+{
+    update_pad(&(pong->left_pad), time);
+    update_pad(&(pong->right_pad), time);
+    update_ball(&(pong->ball), time);
+    bounce_ball(pong);
+}
+
+void render_pong(Pong* pong)
+{
+    render_pad(&(pong->left_pad));
+    render_pad(&(pong->right_pad));
+    render_ball(&(pong->ball));
+}
+
+void set_left_pad_position(Pong* pong, float position)
+{
+    set_pad_position(&(pong->left_pad), position);
+}
+
+void set_left_pad_speed(Pong* pong, float speed)
+{
+    set_pad_speed(&(pong->left_pad), speed);
+}
+
+void set_right_pad_position(Pong* pong, float position)
+{
+    set_pad_position(&(pong->right_pad), position);
+}
+
+void set_right_pad_speed(Pong* pong, float speed)
+{
+    set_pad_speed(&(pong->right_pad), speed);
+}
+
+void bounce_ball(Pong* pong)
+{
+    if (pong->ball.x - pong->ball.radius < 50)
+    {
+        if(((pong->left_pad.y + pong->left_pad.height / 2) - pong->ball.y) < pong->left_pad.height/2 && ((pong->left_pad.y + pong->left_pad.height / 2) - pong->ball.y) > pong->left_pad.height/-2)
+        {
+
+
+        pong->ball.x = pong->ball.radius + 50;
+        pong->ball.speed_x *= -1;
+
+
+        }
+        else
+        {
+
+            pong->ball.speed_x *= -1;
+            pong->ball.x = pong->width / 2;
+            pong->ball.y = pong->height / 2;
+            pong->player2_score += 1;
+            printf("Meccs eredmeny valtozott. Player 1: %d pont, Player 2: %d pont\n",pong->player1_score,pong->player2_score);
+
+        }
+
+    }
+    if (pong->ball.x + pong->ball.radius > pong->width - 50)
+        {
+        if(((pong->right_pad.y + pong->right_pad.height / 2) - pong->ball.y) < pong->right_pad.height/2 && ((pong->right_pad.y + pong->right_pad.height / 2) - pong->ball.y) > pong->right_pad.height/-2)
+        {
+
+
+        pong->ball.x = pong->width - pong->ball.radius - 50;
+        pong->ball.speed_x *= -1;
+        }
+        else
+        {
+            pong->ball.speed_x *= -1;
+            pong->ball.x = pong->width / 2;
+            pong->ball.y = pong->height / 2;
+            pong->player1_score += 1;
+             printf("Meccs eredmeny valtozott. Player 1: %d pont, Player 2: %d pont\n",pong->player1_score,pong->player2_score);
+
+        }
+    }
+    if (pong->ball.y - pong->ball.radius < 0) {
+        pong->ball.y = pong->ball.radius;
+        pong->ball.speed_y *= -1;
+    }
+    if (pong->ball.y + pong->ball.radius > pong->height) {
+        pong->ball.y = pong->height - pong->ball.radius;
+        pong->ball.speed_y *= -1;
+    }
+}
